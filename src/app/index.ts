0a7ebfe8387bdbd116342a4206366426fecb2083
Phillip-Hope Kọláwọlé Òjó <philliphopeojo@gmail.com>
2024-04-03 19:43:37 +0100

    Adding status codes

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import type { StatusCode } from "hono/utils/http-status";
 
-import { BuildSuccessResponse } from "../utils/http.js";
+import { BuildSuccessResponse, HTTPResponse } from "../utils/http.js";
 import adminRoutes from "./admin/index.js";
 import customerRoutes from "./customer/index.js";
 
@@ -10,6 +11,7 @@ const app = new Hono({ strict: true }).basePath("/v1");
 app.use(logger());
 
 app.get("/", (c) => {
+	c.status(HTTPResponse.Success.OK.code as StatusCode);
 	return c.json(BuildSuccessResponse({ message: "App Running", payload: {} }));
 });
 app.route("/", adminRoutes);
